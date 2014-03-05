54e24e235b669c1345f7c3fdbf4b01f9cb5374f4
markov <markov@markov-desktop>
2014-03-05 12:19:25 +0700

    ??????? ? vipgeo

---
+++ Class/Data/Validator/Not/Equal.php
@@ -0,0 +1,15 @@
+/**
+ * Является ли значение поля не идентичным переданому в транспортe
+ * 
+ * @author markov
+ */
+class Data_Validator_Not_Equal extends Data_Validator_Abstract
+{
+    /**
+     * @inheritdoc
+     */
+	public function validate($data, $value)
+	{
+		return $data != $value;
+	}
+}
---
+++ Class/Form/Validator/Error/Min/Length.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Ошибка на максимальную длину
+ * 
+ * @author markov
+ */
+class Form_Validator_Error_Min_Length extends Form_Validator_Error
+{    
+    /**
+     * @inheritdoc
+     */
+    public function errorMessage($value = null) 
+    {
+        $locator = IcEngine::serviceLocator();
+        $min = $this->getParams()[0];
+        $plural = $locator->getService('helperPlural')
+            ->plural($min, 'символа, символов, символов');
+        return 'Длина значения не должна быть меньше ' . $min . ' ' . $plural;
+    }
+}
