2ff81e4b3c3a1b0aa496c7d0ca7400a8996d2d4c
dp <dp@ds.local>
2014-04-14 14:35:36 +0700

    \n\n -> \n

---
+++ Controller/Form/Element/View.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Отображение элемента формы
+ *
+ * @author markov
+ */
+class Controller_Form_Element_View extends Controller_Abstract
+{
+    /**
+     * Отображает элемент формы
+     * 
+     * @Context("loader")
+     */
+    public function index($form, $elementName, $context, $attributes = array())
+    {
+        $element = $form->element($elementName);
+        $element->setAttributes($attributes);
+        $elementType = $element->getType();
+        $path = 'Form/General/' . $elementType;
+        if ($form->name) {
+            $pathByName = 'Form/' . $form->name . '/' . $element->name; 
+            $existsPathByName = $context->loader->findFile(
+                $pathByName  . '.tpl', 'Form'
+            );
+            if ($existsPathByName) {
+                $path = $pathByName;
+            } else {
+                $pathByType = 'Form/' . $form->name . '/General/'. $elementType;
+                $pathExistsByType = $context->loader->findFile(
+                    $pathByType  . '.tpl', 'Form'
+                );
+                if ($pathExistsByType) {
+                    $path = $pathByType;
+                }
+            }
+        }
+        $this->task->setTemplate($path);
+        $this->output->send(array(
+            'element'   => $element
+        ));
+    }
+}
