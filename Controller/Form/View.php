1a039684318bafc743244bd53f465a77c2ee88ef
dp <dp@ds.local>
2014-04-15 14:42:16 +0700

    ????? ?????? ????? ?????????

---
+++ Controller/Form/View.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Контроллер для отрисовки формы
+ *
+ * @author markov
+ */
+class Controller_Form_View extends Controller_Abstract
+{
+    /**
+     * Отрисовывает форму
+     */
+    public function index($form, $content, $context)
+    {
+        if (!trim($content)) {
+            foreach ($form as $element) {
+                $content .= $context->controllerManager->html(
+                    'Form_Element_View/index', array(
+                        'form'          => $form,
+                        'elementName'   => $element->name
+                    )
+                );
+                $content .= $context->controllerManager->html(
+                    'Form_Element_Error/index', array(
+                        'form'          => $form,
+                        'elementName'   => $element->name
+                    )  
+                );
+            }
+        }
+        $this->output->send(array(
+            'form'      => $form,
+            'content'   => $content
+        ));
+    }
+}
