50570b9a2c141636afe455077d76dc831db1bba3
dp <dp@ds.local>
2014-04-16 12:24:42 +0700

    bug fix: ->where('id not in (?)', $flatKeys)

--- Class/Query/Translator/Mysql/Select.php
+++ Class/Query/Translator/Mysql/Select.php
@@ -183,9 +183,10 @@ class Query_Translator_Mysql_Select extends Query_Translator_Abstract
             if (!array_key_exists($i, $values)) {
                 break;
             }
-            $value = is_array($values[$i])
-                ? $this->renderInArray($values[$i])
-                : $helper->quote($values[$i]);
+
+            $value = count($values) > 1
+                ? $this->renderInArray($values)
+                : $helper->quote(reset($values));
             $left = substr($condition, 0, $charPos);
             $right = substr($condition, $charPos + 1);
             $condition = $left . $value . $right;
