018b9236683730258c2ab3c0cb0b18eb943a6845
markov <markov@markov-desktop>
2014-05-12 15:14:25 +0700

    ??????? ? vipgeo

---
+++ Class/Controller/Front/Strategy/Ajax.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Стретегия для обработки ajax запросов
+ * 
+ * @author markov
+ */
+class Controller_Front_Strategy_Ajax extends Controller_Front_Strategy_Abstract
+{
+    /**
+     * @inheritdoc
+     */
+    protected $template = 'Controller/Front/index';
+    
+    /**
+     * @inheritdoc
+     */
+    public function action()
+    {
+        
+    }
+}
