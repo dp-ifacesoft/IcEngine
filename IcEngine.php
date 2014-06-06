3bc9923ab21768de6202419fd870e89359dd8aa3
markov <markov@markov-desktop>
2014-06-06 14:28:30 +0700

    ??????? ??? DDD

--- IcEngine.php
+++ IcEngine.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Класс необходимый для инициализации фреймворка.
  *
@@ -7,40 +6,40 @@
  */
 class IcEngine
 {
-	/**
+    /**
 	 * Загрузчик
 	 *
      * @var Bootstrap_Abstract
 	 */
-	protected static $bootstrap;
+    protected static $bootstrap;
 
-	/**
+    /**
 	 * Экшин фронт контролера по умолчанию
 	 *
      * @var string
 	 */
-	protected static $frontAction = 'index';
+    protected static $frontAction = 'index';
 
-	/**
+    /**
 	 * Фронт контролер по умолчанию
 	 *
      * @var string
 	 */
-	protected static $frontController = 'Front';
+    protected static $frontController = 'Front';
 
-	/**
+    /**
 	 * Название транспорта по умолчанию
 	 *
      * @var string
 	 */
-	protected static $frontInput = 'defaultInput';
+    protected static $frontInput = 'defaultInput';
 
-	/**
+    /**
 	 * Рендер по умолчанию
 	 *
      * @var string
 	 */
-	protected static $frontRender = 'Front';
+    protected static $frontRender = 'Front';
 
     /**
      * Загрузчик
@@ -49,33 +48,33 @@ class IcEngine
      */
     protected static $loader;
 
-	/**
+    /**
 	 * Зарегистрированные менеджеры
 	 *
      * @var array
 	 */
-	protected static $managers = array();
+    protected static $managers = array();
 
     /**
 	 * Путь до движка
 	 *
      * @var string
 	 */
-	protected static $path;
+    protected static $path;
 
-	/**
+    /**
 	 * Путь до корня сайта.
 	 *
      * @var string
 	 */
-	protected static $root;
+    protected static $root;
 
     /**
 	 * Задача фронт контроллера.
 	 *
      * @var Controller_Task
 	 */
-	protected static $task;
+    protected static $task;
 
     /**
      * Сервис локатор
@@ -84,15 +83,15 @@ class IcEngine
      */
     protected static $serviceLocator;
 
-	/**
+    /**
 	 * Получить текущий бутстрап
 	 *
      * @desc Bootstrap_Abstract
 	 */
-	public static function bootstrap()
-	{
-		return self::$bootstrap;
-	}
+    public static function bootstrap()
+    {
+        return self::$bootstrap;
+    }
     
     /**
      * Создать задание для front-контроллера
@@ -105,7 +104,7 @@ class IcEngine
         $task = new Controller_Front_Task($action);
         $viewRenderManager = self::getManager('View_Render');
         $viewRender = $viewRenderManager->byName(self::$frontRender);
-		$task->setViewRender($viewRender);
+        $task->setViewRender($viewRender);
         return $task;
     }
 
@@ -116,41 +115,43 @@ class IcEngine
      */
     protected static function createTaskAction()
     {
-        $action = new Controller_Action(array (
-            'id'			=> null,
-            'controller'	=> self::$frontController,
-            'action'		=> self::$frontAction
-        ));
+        $action = new Controller_Action(
+            array(
+                'id' => null,
+                'controller' => self::$frontController,
+                'action' => self::$frontAction
+            )
+        );
         return $action;
     }
 
-	/**
-	 * Вывод результата работы.
-	 */
-	public static function flush()
-	{
+    /**
+     * Вывод результата работы.
+     */
+    public static function flush()
+    {
         $controllerManager = self::getManager('Controller');
-		$controllerManager->call('Render', 'index', array(
-            'task'  => self::$task
-        ));
-	}
+        $controllerManager->call(
+            'Render', 'index', array('task' => self::$task)
+        );
+    }
 
-	/**
+    /**
 	 * Инициализация лоадера.
 	 *
      * @param string $root Путь до корня сайта.
 	 * @param string $bootstap Путь до загрузчика.
 	 */
-	public static function init($root = null, $bootstap = null)
-	{
-		// Запоминаем путь до движка
-		self::$path = dirname(__FILE__) . '/';
-		if (strlen(self::$path) < 2) {
-			self::$path = '';
-		}
-		// путь до корня сайта
-		self::$root = $root ? rtrim($root, '/\\') . '/' : self::getRoot();
-		self::initLoader();
+    public static function init($root = null, $bootstap = null)
+    {
+        // Запоминаем путь до движка
+        self::$path = dirname(__FILE__) . '/';
+        if (strlen(self::$path) < 2) {
+            self::$path = '';
+        }
+        // путь до корня сайта
+        self::$root = $root ? rtrim($root, '/\\') . '/' : self::getRoot();
+        self::initLoader();
         self::$loader->load('Loader_Auto');
         $autoLoader = new Loader_Auto();
         $autoLoader->register();
@@ -158,66 +159,70 @@ class IcEngine
         self::$loader->setProvider($loaderProvider);
         $configProvider = self::getManager('Data_Provider')->get('Config');
         self::getManager('Config')->setProvider($configProvider);
-		if ($bootstap) {
-			self::initBootstrap($bootstap);
-		}
+        if ($bootstap) {
+            self::initBootstrap($bootstap);
+        }
         self::serviceLocator()->registerService('loader', self::$loader);
-		register_shutdown_function(array(__CLASS__, 'shutdownHandler'));
-	}
+        register_shutdown_function(array(__CLASS__, 'shutdownHandler'));
+    }
 
-	/**
-	 * Подключает загрузчик и запускает его.
-	 *
+    /**
+     * Подключает загрузчик и запускает его.
+     *
      * @param string $path Путь до загрузчика.
-	 */
-	public static function initBootstrap($path)
-	{
-		require $path;
-		$name = basename($path, '.php');
-		require_once __DIR__ . '/Class/Bootstrap/Manager.php';
+     */
+    public static function initBootstrap($path)
+    {
+        require $path;
+        $name = basename($path, '.php');
+        require_once __DIR__ . '/Class/Bootstrap/Manager.php';
         $bootstrapManager = self::getManager('Bootstrap');
-		self::$bootstrap = $bootstrapManager->get($name, $path);
-	}
+        self::$bootstrap = $bootstrapManager->get($name, $path);
+    }
 
-	/**
-	 * Подключение класса Debug
-	 */
-	public static function initDebug()
-	{
-		require dirname(__FILE__) . '/Class/Debug.php';
-		call_user_func_array(array('Debug', 'init'), func_get_args());
-	}
+    /**
+     * Подключение класса Debug
+     */
+    public static function initDebug()
+    {
+        require dirname(__FILE__) . '/Class/Debug.php';
+        call_user_func_array(array('Debug', 'init'), func_get_args());
+    }
 
-	/**
-	 * Инициализация лоадера.
-	 */
-	public static function initLoader()
-	{
-		require dirname(__FILE__) . '/Class/Loader.php';
+    /**
+     * Инициализация лоадера.
+     */
+    public static function initLoader()
+    {
+        require dirname(__FILE__) . '/Class/Loader.php';
         self::$loader = new Loader();
-        self::$loader->addPathes(array(
-			'Class'			=> array(
-				self::$path . 'Class/',
-				self::$path . 'Model/',
-				self::$path
-			),
-			'Controller'	=> array(
-				self::$path . 'Controller/'
-			),
-            'Form'          => array(
-                self::$path . 'View/'
-            ),
-			'Vendor'		=> self::$path . 'Vendor/'
-		));
-	}
+        self::$loader->addPathes(
+            array(
+                'Class' => array(
+                    self::$path . 'Class/',
+                    self::$path . 'Model/',
+                    self::$path . 'Service/',
+                    self::$path
+                ),
+                'Controller' => array(
+                    self::$path . 'Controller/'
+                ),
+                'Form' => array(
+                    self::$path . 'View/'
+                ),
+                'Vendor' => self::$path . 'Vendor/'
+            )
+        );
+    }
 
-	/**
-	 * Подключение класса Tracer
-	 */
-	public static function initTracer()
-	{
+    /**
+     * Подключение класса Tracer
+     */
+    public static function initTracer()
+    {
         require dirname(__FILE__) . '/Class/Tracer.php';
-	}
+    }
+
     /**
      * Получить frontTamplate
      *
@@ -227,6 +232,7 @@ class IcEngine
     {
         return self::$frontTemplate;
     }
+
     /**
      * Получить имя сервиса менеджера для локатора сервисов
      *
@@ -250,15 +256,15 @@ class IcEngine
         return self::$loader;
     }
 
-	/**
-	 * Получить менеджера по имени
-	 *
+    /**
+     * Получить менеджера по имени
+     *
      * @param string $name
-	 * @return Manager_Abstract
-	 */
-	public static function getManager($name)
-	{
-		if (!isset (self::$managers[$name])) {
+     * @return Manager_Abstract
+     */
+    public static function getManager($name)
+    {
+        if (!isset(self::$managers[$name])) {
             $fromServiceLocator = false;
             $serviceName = self::getNameForServiceLocator($name . '_Manager');
             $manager = self::serviceLocator()->getService($serviceName);
@@ -269,22 +275,21 @@ class IcEngine
                 $className = $name . '_Manager';
                 $manager = new $className;
             }
-			self::registerManager($name, $manager, $fromServiceLocator);
-		}
-		return self::$managers[$name];
-	}
+            self::registerManager($name, $manager, $fromServiceLocator);
+        }
+        return self::$managers[$name];
+    }
 
     /**
-	 * Возвращает путь до корня сайта.
-	 *
+     * Возвращает путь до корня сайта.
+     *
      * @return string
-	 */
-	protected static function getRoot()
-	{
-		return isset ($_SERVER['DOCUMENT_ROOT'])
-            ? rtrim($_SERVER['DOCUMENT_ROOT'], '/') . '/'
+     */
+    protected static function getRoot()
+    {
+        return isset($_SERVER['DOCUMENT_ROOT']) ? rtrim($_SERVER['DOCUMENT_ROOT'], '/') . '/' 
             : rtrim(realpath(self::$path . '..'), '/') . '/';
-	}
+    }
 
     /**
      * Получить сервис локатор
@@ -295,7 +300,7 @@ class IcEngine
     {
         return self::$serviceLocator;
     }
-    
+
     /**
      * Получить задание фронт контроллера
      * 
@@ -306,49 +311,48 @@ class IcEngine
         return self::$task;
     }
 
-	/**
-	 * Путь до корня движка
-	 *
+    /**
+     * Путь до корня движка
+     *
      * @return string
-	 */
-	public static function path()
-	{
-		return self::$path;
-	}
+     */
+    public static function path()
+    {
+        return self::$path;
+    }
 
-	/**
-	 * Зарегистрировать нового менеджера по имени
-	 *
+    /**
+     * Зарегистрировать нового менеджера по имени
+     *
      * @param string $name
-	 * @param Manager_Abstract $manager
+     * @param Manager_Abstract $manager
      * @param boolean $fromServiceLocator
-	 */
-	public static function registerManager($name, $manager,
-        $fromServiceLocator = false)
-	{
-		self::$managers[$name] = $manager;
+     */
+    public static function registerManager($name, $manager, $fromServiceLocator = false)
+    {
+        self::$managers[$name] = $manager;
         if (!$fromServiceLocator) {
             $serviceName = self::getNameForServiceLocator($name);
             self::$serviceLocator->registerService($serviceName, $manager);
         }
-	}
+    }
 
-	/**
-	 * Путь до корня сайта.
-	 *
+    /**
+     * Путь до корня сайта.
+     *
      * @return string
-	 */
-	public static function root()
-	{
-		return self::$root;
-	}
+     */
+    public static function root()
+    {
+        return self::$root;
+    }
 
-	/**
-	 * Запуск рабочего цикла и вывод результата.
-	 */
-	public static function run()
-	{
-		self::$bootstrap->run();
+    /**
+     * Запуск рабочего цикла и вывод результата.
+     */
+    public static function run()
+    {
+        self::$bootstrap->run();
         if (!self::$task) {
             self::$task = self::createFrontControllerTask();
         }
@@ -358,13 +362,12 @@ class IcEngine
         self::$task->setStrategies(self::$bootstrap->getStrategies());
         try {
             $controllerManager->call(
-                self::$frontController, self::$frontAction, $transport,
-                self::$task
+                self::$frontController, self::$frontAction, $transport, self::$task
             );
         } catch (Exception $e) {
             die;
         }
-	}
+    }
 
     /**
      * Получить локатор сервисов
@@ -472,21 +475,22 @@ class IcEngine
     /**
      * Обрабочик завершения приложения
      */
-	public static function shutdownHandler ()
-	{
-		$error = error_get_last();
+    public static function shutdownHandler()
+    {
+        $error = error_get_last();
         $resourceManager = self::getManager('Resource');
-		if (!$error) {
-			$resourceManager->save();
+        if (!$error) {
+            $resourceManager->save();
             $shutdownManager = self::getManager('Shutdown');
             $shutdownManager->process();
-		} else {
-			$errno = $error['type'];
-			if ($errno == E_ERROR || $errno == E_USER_ERROR) {
-				if (!headers_sent()) {
-					header('HTTP/1.0 500 Internal Server Error');
-				}
-			}
-		}
-	}
+        } else {
+            $errno = $error['type'];
+            if ($errno == E_ERROR || $errno == E_USER_ERROR) {
+                if (!headers_sent()) {
+                    header('HTTP/1.0 500 Internal Server Error');
+                }
+            }
+        }
+    }
+
 }
