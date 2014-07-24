d687e20c2e891d9f4bc7c41ba5c256bfb42dd6a1
sergb <sergb@localhost>
2014-07-24 13:32:34 +0700

    ?????????? #60172 ?????? ?????? ? ?????????????? ???????? ? Redis

--- Class/Data/Provider/Redis.php
+++ Class/Data/Provider/Redis.php
@@ -1,24 +1,22 @@
 <?php
-
 /**
  * Провайдер данных Redis
  *
+ * @link https://github.com/nicolasff/phpredis
  * @author goorus, morph
  */
 class Data_Provider_Redis extends Data_Provider_Abstract
 {
-
     /**
      * Подключение к редису
      *
      * @var Redis
      */
     protected $connections = array();
-    
     /**
      * Добавление значения к ключу.
      * Атомарная операция.
-     * 
+     *
      * @param string $key Ключ
      * @param string $value Строка, которая будет добавлена к текущему значению ключа.
      * @return int Длина нового значения (строки)
@@ -28,41 +26,31 @@ class Data_Provider_Redis extends Data_Provider_Abstract
         if ($this->tracer) {
             $this->tracer->add('append', $key);
         }
-        return $this->getConnection($key)->append(
-                $this->keyEncode($key),
-                $value
-        );
+        return $this->getConnection($key)->append($this->keyEncode($key), $value);
     }
-    
     /**
      * @inheritdoc
      */
     protected function _setOption($key, $value)
     {
         switch ($key) {
-            case 'servers':
-                foreach ($value as $server) {
-                    $redis = new Redis();
-                    $this->connections[$server['host']] = $redis;
-                    $redis->connect(
-                        $server['host'], isset($server['port']) ? $server['post'] : null
-                    );
-                }
-                return true;
+        case 'servers':
+            foreach ($value as $server) {
+                $redis = new Redis();
+                $this->connections[$server['host']] = $redis;
+                $redis->connect($server['host'], isset($server['port']) ? $server['post'] : null);
+            }
+            return true;
         }
         return parent::_setOption($key, $value);
     }
-
     /**
      * @inheritdoc
      */
     public function decrement($key, $value = 1)
     {
-        return $this->getConnection($key)->decrBy(
-                        $this->keyEncode($key), $value
-        );
+        return $this->getConnection($key)->decrBy($this->keyEncode($key), $value);
     }
-
     /**
      * @inheritdoc
      */
@@ -72,10 +60,8 @@ class Data_Provider_Redis extends Data_Provider_Abstract
             if (Tracer::$enabled) {
                 $startTime = microtime(true);
             }
-
             $connection = $this->getConnection($keys);
             $result = $connection->delete($this->keyEncode($keys));
-
             if (Tracer::$enabled) {
                 $endTime = microtime(true);
                 Tracer::incRedisDeleteCount();
@@ -83,13 +69,10 @@ class Data_Provider_Redis extends Data_Provider_Abstract
             }
             return $result;
         }
-
         foreach ($keys as $key) {
             $this->delete($key);
         }
-
     }
-
     /**
      * @inheritdoc
      */
@@ -97,7 +80,6 @@ class Data_Provider_Redis extends Data_Provider_Abstract
     {
         $this->delete($this->keys($pattern));
     }
-
     /**
      * Отфильтровать ключу для конкретного соединения
      *
@@ -108,13 +90,10 @@ class Data_Provider_Redis extends Data_Provider_Abstract
     protected function filterKeys($keys, $index)
     {
         $count = count($this->connections);
-
         if ($count == 1) {
             return $keys;
         }
-
         $result = array();
-
         foreach ($keys as $key) {
             $keyIndex = abs(crc32($key)) % $count;
             if ($keyIndex == $index) {
@@ -123,7 +102,6 @@ class Data_Provider_Redis extends Data_Provider_Abstract
         }
         return $result;
     }
-
     /**
      * @inheritdoc
      */
@@ -132,11 +110,8 @@ class Data_Provider_Redis extends Data_Provider_Abstract
         if (Tracer::$enabled) {
             $startTime = microtime(true);
         }
-
         $connection = $this->getConnection($key);
-
         $result = $connection->get($this->keyEncode($key));
-
         if (Tracer::$enabled) {
             $endTime = microtime(true);
             Tracer::incRedisGetCount();
@@ -149,9 +124,7 @@ class Data_Provider_Redis extends Data_Provider_Abstract
             $value = $result;
         }
         return $value;
-
     }
-
     /**
      * Получить соединение (сокет)
      *
@@ -161,17 +134,21 @@ class Data_Provider_Redis extends Data_Provider_Abstract
     public function getConnection($key)
     {
         $count = count($this->connections);
-
         if ($count == 1) {
             return reset($this->connections);
         }
-
         $index = abs(crc32($key)) % $count;
-
         return $this->connections[$index];
-
     }
-
+    /**
+     * Получить основное соединение (сокет)
+     *
+     * @return resource
+     */
+    public function getMainConnection()
+    {
+        return reset($this->connections);
+    }
     /**
      * @inheritdoc
      */
@@ -184,40 +161,28 @@ class Data_Provider_Redis extends Data_Provider_Abstract
             }
             return array($keys[0] => $value);
         }
-
         $result = array();
         $keys = array_map(array($this, 'keyEncode'), $keys);
-
         foreach ($this->connections as $i => $connection) {
             $connectionKeys = $this->filterKeys($keys, $i);
             if (!$connectionKeys) {
                 continue;
             }
-
             $items = $connection->mGet($connectionKeys);
             if (!$items) {
                 return;
             }
-
-            $result = array_merge($result, array_combine(
-                            $connectionKeys, $items
-            ));
+            $result = array_merge($result, array_combine($connectionKeys, $items));
         }
-
         $sortedItems = array();
-
         foreach ($keys as $key) {
             $sortedItems[$key] = isset($result[$key]) ? $this->valueDecode($result[$key]) : null;
         }
-
         if ($numericIndex) {
             return array_values($sortedItems);
         }
-
         return $sortedItems;
-
     }
-    
     /**
      * Получение текущего значения ключа с одновременной заменой на новое значение.
      * Атомарная операция.
@@ -236,17 +201,13 @@ class Data_Provider_Redis extends Data_Provider_Abstract
         }
         return $value;
     }
-
     /**
      * @inheritdoc
      */
     public function increment($key, $value = 1)
     {
-        return $this->getConnection($key)->incrBy(
-                        $this->keyEncode($key), $value
-        );
+        return $this->getConnection($key)->incrBy($this->keyEncode($key), $value);
     }
-
     /**
      * @inheritdoc
      */
@@ -254,7 +215,6 @@ class Data_Provider_Redis extends Data_Provider_Abstract
     {
         return $this->prefix . $key;
     }
-
     /**
      * @inheritdoc
      */
@@ -262,7 +222,6 @@ class Data_Provider_Redis extends Data_Provider_Abstract
     {
         return substr($key, strlen($this->prefix));
     }
-
     /**
      * @inheritdoc
      */
@@ -271,35 +230,25 @@ class Data_Provider_Redis extends Data_Provider_Abstract
         if (Tracer::$enabled) {
             $startTime = microtime(true);
         }
-
         $keys = array();
-
         foreach ($this->connections as $connection) {
             if (strlen($pattern) > 1 && $pattern[strlen($pattern) - 1] === '*') {
                 $pattern = rtrim($pattern, '*');
             }
-
             $key = $this->keyEncode($pattern) . '*';
             $connectionKeys = $connection->keys($key);
-
             if (!$connectionKeys) {
                 continue;
             }
-
             $keys = array_merge($keys, $connectionKeys);
-
         }
-
         if (Tracer::$enabled) {
             $endTime = microtime(true);
             Tracer::incRedisKeyCount();
             Tracer::incRedisKeyTime($endTime - $startTime);
         }
-
         return array_map(array($this, 'keyDecode'), $keys);
-
     }
-
     /**
      * @inheritdoc
      */
@@ -309,7 +258,6 @@ class Data_Provider_Redis extends Data_Provider_Abstract
             $connection->publish($channel, $message);
         }
     }
-
     /**
      * @inheritdoc
      */
@@ -322,30 +270,24 @@ class Data_Provider_Redis extends Data_Provider_Abstract
         if ($expiration < 0) {
             $expiration = 0;
         }
-
         if (Tracer::$enabled) {
             $startTime = microtime(true);
         }
-
         $connection = $this->getConnection($key);
         $value = $this->valueEncode($value);
         $key = $this->keyEncode($key);
-
         if ($expiration) {
             $result = $connection->setex($key, $expiration, $value);
         } else {
             $result = $connection->set($key, $value);
         }
-
         if (Tracer::$enabled) {
             $endTime = microtime(true);
             Tracer::incRedisSetCount();
             Tracer::incRedisSetTime($endTime - $startTime);
         }
-
         return $result;
     }
-
     /**
      * @inheritdoc
      */
@@ -355,7 +297,6 @@ class Data_Provider_Redis extends Data_Provider_Abstract
             $connection->subscribe($channel);
         }
     }
-
     /**
      * @inheritdoc
      */
@@ -388,4 +329,195 @@ class Data_Provider_Redis extends Data_Provider_Abstract
         return json_encode($value, JSON_UNESCAPED_UNICODE);
     }
 
+    /**
+     * Add one or more members to a sorted set or update its score if it already exists
+     * Example: zAdd($key, $score1, $value1, $score2, $value2, ...)
+     */
+    public function zAdd($key, $score, $value)
+    {
+        return $this->getMainConnection()->zAdd(func_get_args());
+    }
+    /**
+     * Get the number of members in a sorted set
+     */
+    public function zSize($key)
+    {
+        return $this->getMainConnection()->zSize($key);
+    }
+    /**
+     *  Count the members in a sorted set with scores within the given values
+     */
+    public function zCount($key, $minScore, $maxScore)
+    {
+        return $this->getMainConnection()->zCount($key, $minScore, $maxScore);
+    }
+    /**
+     *
+     * Increment the score of a member in a sorted set
+     */
+    public function zIncrBy($key, $incValue, $member)
+    {
+        return $this->getMainConnection()->zIncrBy($key, $incValue, $member);
+    }
+    /**
+     * Intersect multiple sorted sets and store the resulting sorted set in a new key
+     * 
+     * @param string $keyOutput
+     * @param array $zSetKeys
+     * @param array $weights
+     * @param string $aggregateFunction ("SUM", "MIN", or "MAX"), "SUM" is default
+     */
+    public function zIntersect($keyOutput, array $zSetKeys, array $weights, $aggregateFunction=null)
+    {
+        return $this->getMainConnection()->zInter($keyOutput, $zSetKeys, $weights, $aggregateFunction);
+    }
+    /**
+     * Return a range of members in a sorted set, by index.
+     * Start and stop are interpreted as zero-based indices: 
+     * 0 the first element, 1 the second ... -1 the last element, -2 the penultimate ...
+     */
+    public function zRange($key, $start, $end, $withScores=false)
+    {
+        return $this->getMainConnection()->zRange($key, $start, $end, $withScores);
+    }
+    /**
+     * Return a range of members in a sorted set, by index, with scores ordered from high to low.
+     * Start and stop are interpreted as zero-based indices: 
+     * 0 the first element, 1 the second ... -1 the last element, -2 the penultimate ...
+     * 
+     * @param string $key
+     * @param int $start
+     * @param int $end
+     * @param bool $withScores default=false
+     * @return array Values in specified range.
+     */
+    public function zRevRange($key, $start, $end, $withScores=false)
+    {
+        return $this->getMainConnection()->zRevRange($key, $start, $end, $withScores);
+    }
+    /**
+     * Returns the elements of the sorted set stored at the specified key which have scores in the range [start,end]. 
+     * Adding a parenthesis before start or end excludes it from the range. +inf and -inf are also valid limits. 
+     */
+    public function zRangeByScore($key, $start, $end, $limit=null, $offset=null, $withScores=false)
+    {
+        $options = [];
+        if (isset($limit, $offset)) {
+            $options['limit'] = [$offset, $limit];
+        }
+        if ($withScores) {
+            $options['withscores'] = true;
+        }
+        if (empty($options)) {
+            $options = null;
+        }
+        if (is_null($start)) {
+            $start = '-inf';
+        }
+        if (is_null($end)) {
+            $end = '+inf';
+        }
+        return $this->getMainConnection()->zRangeByScore($key, $start, $end, $options);
+    }
+    /**
+     * Returns the elements of the sorted set stored at the specified key which have scores in the range [start,end]. 
+     * Adding a parenthesis before start or end excludes it from the range. +inf and -inf are also valid limits. 
+     * zRevRangeByScore returns items in reverse order, when the start and end parameters are swapped.     
+     */
+    public function zRevRangeByScore($key, $start, $end, $limit=null, $offset=null, $withScores=false)
+    {
+        $options = [];
+        if (isset($limit, $offset)) {
+            $options['limit'] = [$offset, $limit];
+        }
+        if ($withScores) {
+            $options['withscores'] = true;
+        }
+        if (empty($options)) {
+            $options = null;
+        }
+        if (is_null($start)) {
+            $start = '+inf';
+        }
+        if (is_null($end)) {
+            $end = '-inf';
+        }
+        return $this->getMainConnection()->zRevRangeByScore($key, $start, $end, $options);
+    }
+    /**
+     * Determine the index of a member in a sorted set,
+     * starting at 0 for the item with the smallest score.
+     * 
+     * @param string $key
+     * @param string $member
+     * @return int
+     */
+    public function zRank($key, $member)
+    {
+        return $this->getMainConnection()->zRank($key, $member);
+    }
+    /**
+     * Determine the index of a member in a sorted set.
+     * zRevRank starts at 0 for the item with the largest score.
+     */
+    public function zRevRank($key, $member)
+    {
+        return $this->getMainConnection()->zRevRank($key, $member);
+    }
+    /**
+     * Remove one or more members from a sorted set.
+     * Example: zDelete($key, $member1, $member2, $member3)
+     * 
+     * @param string $key
+     * @param string $member
+     * @return int
+     */
+    public function zDelete($key, $member)
+    {
+        return $this->getMainConnection()->zDelete(func_get_args());
+    }
+    /**
+     * Remove all members in a sorted set within the given indexes
+     */
+    public function zDeleteRangeByRank($key, $start, $end)
+    {
+        return $this->getMainConnection()->zDeleteRangeByRank($key, $start, $end);
+    }
+    /**
+     * Remove all members in a sorted set within the given scores
+     * 
+     * @param string $key
+     * @param double $start  "+inf" or "-inf" string also valit
+     * @param double $end    "+inf" or "-inf" string also valit
+     */
+    public function zDeleteRangeByScore($key, $start, $end)
+    {
+        return $this->getMainConnection()->zDeleteRangeByScore($key, $start, $end);
+    }
+    /**
+     * Get the score associated with the given member in a sorted set
+     * 
+     * @param string $key
+     * @param string $member
+     * @return double
+     */
+    public function zScore($key, $member)
+    {
+        return $this->getMainConnection()->zScore($key, $member);
+    }
+    /**
+     * Add multiple sorted sets and store the resulting sorted set in a new key
+     * Creates an union of sorted sets given in second argument. 
+     * The result of the union will be stored in the sorted set defined by the first argument.
+     * 
+     * @param string $keyOutput
+     * @param array $zSetKeys
+     * @param array $weights
+     * @param string $aggregateFunction ("SUM", "MIN", or "MAX"), "SUM" is default
+     * @return int The number of values in the new sorted set.
+     */
+    public function zUnion($keyOutput, array $zSetKeys, array $weights, $aggregateFunction=null)
+    {
+        return $this->getMainConnection()->zUnion($keyOutput, $zSetKeys, $weights, $aggregateFunction);
+    }
 }
