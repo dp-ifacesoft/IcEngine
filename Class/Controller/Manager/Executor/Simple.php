ae34e850b4fc6d8b40b6e4d1c1c27c9f2a9aa9fc
sergb <sergb@localhost>
2014-09-15 13:50:41 +0700

    ??????? ? gdekvartira

---
+++ Class/Controller/Manager/Executor/Simple.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Простой "выполнить" действий контроллера
+ */
+class Controller_Manager_Executor_Simple extends 
+    Controller_Manager_Executor_Abstract
+{
+    /**
+     * @inheritdoc
+     */
+    public function execute($callable, $args)
+    {
+        call_user_func_array($callable, $args);
+    }
+
+
+}
