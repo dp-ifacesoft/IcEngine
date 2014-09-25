8fa2bd6efab4c7620caff166564fbd0b6e8fdd6a
LiverEnemy <liverenemy@gmail.com>
2014-09-25 15:41:55 +0700

    ???????? ????????? ?? ??????

--- Validator/Error/Rest/Api/Allows/Http/Method.php
+++ Validator/Error/Rest/Api/Allows/Http/Method.php
@@ -23,6 +23,10 @@ class Validator_Error_Rest_Api_Allows_Http_Method extends Validator_Error_Rest_A
      */
     public function errorMessage($value = null)
     {
-        return 'This HTTP method is not allowed in ' . $this->_getRestApiName() . '::' . $this->_getRestApiAction();
+        /** @var Request $request */
+        $request = $this->getService('request');
+        $httpMethod = $request->requestMethod();
+        return 'HTTP method \'' . $httpMethod . '\' is not allowed in '
+        . $this->_getRestApiName() . '::' . $this->_getRestApiAction();
     }
 } 
