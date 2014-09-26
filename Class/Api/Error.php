0307d02ee0597a14223f98e0788837f1f46e903e
sergb <sergb@localhost>
2014-09-26 17:03:16 +0700

    BOM remove

---
+++ Class/Api/Error.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Ошибки к api
+ *
+ * @author markov
+ * @Service("apiError")
+ */
+class Api_Error
+{
+    const COMMAND_NOT_FOUND = 1;
+    const ACCESS_DENIED = 2;
+    const NOT_ENOUGH_PARAMS = 3;
+    
+    /**
+     * Получить описание ошибки
+     * 
+     * @param integer $error код ошибки
+     */
+    public function getErrorDescription($error)
+    {
+        static $descriptions = array(
+            self::COMMAND_NOT_FOUND => 'Команда не найдена',
+            self::ACCESS_DENIED     => 'Доступ запрещен',
+            self::NOT_ENOUGH_PARAMS => 'Недостаточно параметров'
+        );
+        return isset($descriptions[$error]) ? $descriptions[$error] : null;
+    }
+}
---
+++ Vendor/smarty_plugins/function.Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Плагин смарти для вызова контроллера.
+ *
+ * @param array $params
+ * @return string
+ */
+function smarty_function_Controller(array $params)
+{
+    $serviceLocator = IcEngine::serviceLocator();
+    $controllerManager = $serviceLocator->getService('controllerManager');
+    $controllerParams = $params;
+    unset($controllerParams['call']);
+    if (isset($params['controllerParams'])) {
+        $controllerParams = $params['controllerParams'];
+    }
+    return $controllerManager->html($params['call'], $controllerParams);
+}
