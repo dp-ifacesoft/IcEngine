7e420323e73fd19f3ff40032864a3aa4409b3b04
markov <markov@localhost>
2014-11-06 20:06:00 +0700

    fix

--- Class/Data/Driver/Mysqli/Redis.php
+++ Class/Data/Driver/Mysqli/Redis.php
@@ -19,6 +19,30 @@ class Data_Driver_Mysqli_Redis extends Data_Driver_Abstract
      */
 	public function execute(\Query_Abstract $query, $options = null)
     {
+        $dataProvider = App::dataProviderManager()->get('Mysqli_Redis');
+        echo $query->translate();
+        $queryBase = App::queryBuilder()
+            ->select('id');
+        $from = $query->getPart(Query::FROM);
+        $queryBase->setPart(Query::FROM, $from);
+        $hashs = [];
+        foreach ($query->getPart(Query::WHERE) as $item) {
+            echo ' <pre>' . print_r($item, 1) . '</pre>'; 
+            $newQuery = clone $queryBase;
+            $newQuery->setPart(Query::WHERE, [
+                $item
+            ]);
+            echo $newQuery->translate();
+            $hash = md5($newQuery->translate());
+            $hashs[] = $hash;
+            if (!$dataProvider->exists($hash)) {
+                $values = $this->sourceDriver->execute($query, $options)->asColumn();
+                $dataProvider->sAdd($hash, $values);
+            }
+        }
+        $data = $dataProvider->sInter($hashs);
+        print_r($data);
+        die();
         return $this->sourceDriver->execute($query, $options);
     }
     
