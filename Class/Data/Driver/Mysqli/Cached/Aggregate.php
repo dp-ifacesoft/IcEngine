5a04fc0a028592ae53b1ab32d3b6d28af0962c99
markov <markov@localhost>
2014-11-10 18:52:26 +0700

    fix

--- Class/Data/Driver/Mysqli/Cached/Aggregate.php
+++ Class/Data/Driver/Mysqli/Cached/Aggregate.php
@@ -141,7 +141,7 @@ class Data_Driver_Mysqli_Cached_Aggregate extends Data_Driver_Abstract
                 ];
             }
         }
-        $zArrayValuesChunk = array_chunk($zArrayValues, 100000);
+        $zArrayValuesChunk = array_chunk($zArrayValues, 10000);
         foreach ($zArrayValuesChunk as $item) {
             $dataProvider->zAddArray($hash, $item);
         }
