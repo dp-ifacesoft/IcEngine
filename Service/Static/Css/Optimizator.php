ae7a349203d212401c1d01fe4373a6b417a60705
markov <markov@localhost>
2015-02-06 13:57:34 +0700

    fix

--- Service/Static/Css/Optimizator.php
+++ Service/Static/Css/Optimizator.php
@@ -15,7 +15,7 @@ class Service_Static_Css_Optimizator extends Service_Abstract
      */
     public function run($path)
     {
-        //$path = IcEngine::root() . 'IcEngine/Static/nodejs';
-        //exec('cd ' . $path . ' && node cssoRun "' . $path . '" > /dev/null &');
+        $path = IcEngine::root() . 'IcEngine/Static/nodejs';
+        exec('cd ' . $path . ' && node cssoRun "' . $path . '" > /dev/null &');
     }
 }
